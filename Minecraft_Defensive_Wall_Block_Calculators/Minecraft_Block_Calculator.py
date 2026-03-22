4a1940c1f91245b18976493a114b2410e542bb2a
TurfClemberg <turf_clemberg@outlook.com>
2026-03-22 14:10:52 +0530

    Simplify intersecting blocks calculation in Minecraft Block Calculator

--- Minecraft_Defensive_Wall_Block_Calculators/Minecraft_Block_Calculator.py
+++ Minecraft_Defensive_Wall_Block_Calculators/Minecraft_Block_Calculator.py
@@ -18,12 +18,7 @@ length_length = int(input("Enter the length of AB of your wall (In Blocks): "))
 length_breadth = int(input("Enter the length of AD of your wall (In Blocks): "))
 width_length = int(input("Enter the width of AB of your wall (In Blocks): "))
 width_breadth = int(input("Enter the width of AD of your wall (In Blocks): "))
-if height_length == height_breadth:
-    intersecting_blocks = (width_length * width_breadth * height_length)
-elif height_length > height_breadth:
-    intersecting_blocks = (width_length * width_breadth * height_breadth)
-else:
-    intersecting_blocks = (width_length * width_breadth * height_length)
+intersecting_blocks = ((width_length * width_breadth) * min(height_length, height_breadth))
 total_blocks_for_length = (length_length * height_length * width_length) * 2
 total_blocks_for_breadth = ((length_breadth * height_breadth * width_breadth) - intersecting_blocks * 2) * 2
 total_blocks = total_blocks_for_length + total_blocks_for_breadth
